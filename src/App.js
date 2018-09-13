695c5300ff05d8fadebea00c7b95bfcc4378b18f
Himanshu <himanshumishra3@gmail.com>
2018-09-13 21:15:01 +0530

    multiple edit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const postList = [[0, 'sport', 'himanshu'], [1, 'fashion', 'deepika'], [2, 'politics', 'rahul']]
+const postList = [ {key: 0, postName: 'sport', authorName: 'himanshu'}, {key: 1, postName: 'fashion', authorName: 'deepika'}, {key: 2, postName: 'politics', authorName: 'rahul'}]
 
 class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { edit: false };
+    this.handleEditClick = this.handleEditClick.bind(this); // check how to remove bind
+  }
+
+  handleEditClick() {
+    this.setState({ edit: !this.state.edit })
+  }
+
+  authorAndPostName() {
+    return this.props.post.postName + ' ' + this.props.post.authorName
+  }
+
   render() {
-    function editPost(key, post, handleSubmit, handleInputChange) {
-      if (key.length != 0 && key.includes(post[0].toString()) ) {
-        return <PostForm editPost="true" authorName={post[2]} postName={post[1]} onSubmit={handleSubmit} onChange={handleInputChange} />;
-      }
-    }
-    const lists = this.props.postList.map((post, index) =>
-      <li key={post[0]} id={post[0]}>{ post[1] + ' ' + post[2] }
-        <button onClick={this.props.handleEditClick}>Edit</button>
-        <button onClick={this.props.onDelete}>Delete</button>
-        {editPost(this.props.editPostkey, post, this.props.onSubmit, this.props.onChange)}
+    return (
+      <li key={this.props.post.key} >{ this.authorAndPostName() }
+        <button onClick={this.handleEditClick}>Edit</button>
+        <button onClick={(event, postKey) => this.props.onDelete(event, this.props.post.key)}>Delete</button>
+        {this.state.edit ? <PostForm post={this.props.post} onSubmit={(event, post) => this.props.onSubmit(post, event)} /> : '' }
       </li>
+    );
+  }
+}
+
+class PostDisplayList extends React.Component {
+  render() {
+    const lists = this.props.postList.map((post, index) =>
+      <List post={post} onDelete={this.props.onDelete} onSubmit={(post, event) => this.props.onSubmit(post, event)} />
     );
     return (
       <ul>{lists}</ul>
@@ -24,17 +41,36 @@ class List extends React.Component {
 }
 
 class PostForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { key: this.props.post.key, postName: this.props.post.postName, authorName: this.props.post.authorName }
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(event) {
+    if(event.target.className === 'postName') {
+      this.setState({ postName: event.target.value});
+    } else {
+      this.setState({ authorName: event.target.value});
+    }
+  }
+
+  onSubmit(event) {
+    this.props.onSubmit(event, this.state);
+  }
+
   render() {
     return (
-      <form onSubmit={this.props.onSubmit}>
+      <form onSubmit={this.onSubmit}>
         <label>
           Title:
-          <input type="text" className='postName' onChange={this.props.onChange} />
+          <input type="text" className='postName' onChange={this.onChange} value={this.state.postName}/>
         </label>
         <br />
         <label>
           Author Name:
-          <input type="text" className='authorName' onChange={this.props.onChange}/>
+          <input type="text" className='authorName' onChange={this.onChange} value={this.state.authorName}/>
         </label>
         <br />
         <input type="submit" value="Submit" />
@@ -46,40 +82,23 @@ class PostForm extends React.Component {
 class InitialLayout extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { newForm: false, postArray: postList, authorName: '', postName: '', editPost: false, editPostkey: [] }
+    this.state = { newForm: false, postArray: postList }
     this.handleClick = this.handleClick.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.count = this.state.postArray.length;
     this.onDelete = this.onDelete.bind(this);
-    this.handleEditClick = this.handleEditClick.bind(this);
   }
 
   handleClick(event) {
     this.setState({ newForm: !this.state.newForm })
   }
 
-  handleInputChange(event) {
-    if(event.target.className === 'postName') {
-      this.setState({ postName: event.target.value});
-    } else {
-      this.setState({ authorName: event.target.value});
-    }
-  }
-
-  handleSubmit(event) {
+  onSubmit(event, post) {
     let posts = this.state.postArray;
-    if(this.state.editPost) {
-      let postKey = event.target.parentElement.id
-      let postIndex = this.findKeyIndex(postKey);
-      posts[postIndex][1] = this.state.postName;
-      posts[postIndex][2] = this.state.authorName;
-      let editPostKeyArray = this.state.editPostkey;
-      editPostKeyArray.splice( editPostKeyArray.indexOf(postKey), 1 )
-      this.setState({editPost: false, editPostkey: editPostKeyArray})
+    if(post.key == 0 || post.key) {
+      let postIndex = this.findKeyIndex(post.key);
+      posts[postIndex].postName = post.postName;
+      posts[postIndex].authorName = post.authorName;
     } else {
-      posts.push([this.count, this.state.postName, this.state.authorName])
-      this.count += 1;
+      posts.push({key: this.state.postArray.length, postName: post.postName, authorName: post.authorName});
     }
     this.setState({ postArray: posts});
     event.preventDefault();
@@ -87,20 +106,14 @@ class InitialLayout extends React.Component {
 
   findKeyIndex(key) {
     for(var i = 0; i < this.state.postArray.length; i++) {
-      if (this.state.postArray[i][0] == key) {
+      if (this.state.postArray[i].key == key) {
         return i;
       }
     }
   }
 
-  handleEditClick(event) {
-    let editPostkeys = this.state.editPostkey;
-    editPostkeys.push(event.target.parentElement.id);
-    this.setState({editPost: true, editPostkey: editPostkeys})
-  }
-
-  onDelete(event) {
-    let keyValueIndex = this.findKeyIndex(event.target.parentElement.id)
+  onDelete(event, postKey) {
+    let keyValueIndex = this.findKeyIndex(postKey)
     let posts = Object.assign([], this.state.postArray);
     posts.splice(keyValueIndex, 1);
     this.setState({postArray: posts});
@@ -109,8 +122,9 @@ class InitialLayout extends React.Component {
   render() {
     let form;
     let button;
+    let post= {authorName: '', postName: ''};
     if (this.state.newForm) {
-      form = <PostForm onSubmit={this.handleSubmit} onChange={this.handleInputChange}  />;
+      form = <PostForm onSubmit={(event, post) => this.onSubmit(event, post)} post={post} />;
       button = <button onClick={this.handleClick}> Hide Form </button>;
     } else {
       form = '';
@@ -120,7 +134,7 @@ class InitialLayout extends React.Component {
       <div>
         {form}
         {button}
-        <List postList={this.state.postArray} onDelete={this.onDelete} editPostkey={this.state.editPostkey} editPost={this.state.editPost} handleEditClick={this.handleEditClick} onSubmit={this.handleSubmit} onChange={this.handleInputChange} />
+        <PostDisplayList postList={this.state.postArray} onDelete={this.onDelete} onSubmit={(event, arg) => this.onSubmit(arg, event)} />
       </div>
     );
   }
