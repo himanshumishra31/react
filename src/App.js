83d00f048e46f7e5d5a3107cd1b124dbdec1488a
Himanshu <himanshumishra3@gmail.com>
2018-09-14 16:43:47 +0530

    sorting added

--- src/App.js
+++ src/App.js
@@ -29,12 +29,38 @@ class List extends React.Component {
 }
 
 class PostDisplayList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { sortByPost: false, sortByAuthor: false }
+  }
+
+  onCheckboxChange = (event) => {
+    if(event.target.className === "postNameCheckbox") {
+      this.setState({ sortByPost: true, sortByAuthor: false });
+    } else {
+      this.setState({ sortByAuthor: true, sortByPost: false });
+    }
+    this.props.onCheckboxChange(event);
+  }
+
   render() {
     const lists = this.props.postList.map((post, index) =>
       <List post={post} onDelete={this.props.onDelete} onSubmit={(post, event) => this.props.onSubmit(post, event)} />
     );
     return (
-      <ul>{lists}</ul>
+      <div>
+        <h3>Sort By</h3>
+        <label>
+          Post Name
+          <input type="checkbox" className="postNameCheckbox" onChange={this.onCheckboxChange} checked={this.state.sortByPost} />
+        </label>
+        <label>
+          Author Name
+          <input type="checkbox" className="authorNameCheckbox" onChange={this.onCheckboxChange} checked={this.state.sortByAuthor}/>
+        </label>
+        <ul>{lists}</ul>
+      </div>
+
     );
   }
 }
@@ -79,11 +105,38 @@ class PostForm extends React.Component {
 class InitialLayout extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { newForm: false, postArray: postList }
+    this.state = { newForm: false, postArray: postList, sortByAuthor: false, sortByPost: false }
+    this.onCheckboxChange = this.onCheckboxChange.bind(this);
   }
 
   handleClick = (event) => {
     this.setState({ newForm: !this.state.newForm })
+  }
+
+  comparePostName(a,b) {
+    if (a.postName < b.postName)
+      return -1;
+    if (a.postName > b.postName)
+      return 1;
+    return 0;
+  }
+
+  compareAuthorName(a,b) {
+    if (a.authorName < b.authorName)
+      return -1;
+    if (a.authorName > b.authorName)
+      return 1;
+    return 0;
+  }
+
+  onCheckboxChange = (event) => {
+    let posts = this.state.postArray;
+    if(event.target.className === "postNameCheckbox") {
+      posts.sort(this.comparePostName);
+    } else {
+      posts.sort(this.compareAuthorName);
+    }
+    this.setState({ postArray: posts });
   }
 
   onSubmit(event, post) {
@@ -129,7 +182,7 @@ class InitialLayout extends React.Component {
       <div>
         {form}
         {button}
-        <PostDisplayList postList={this.state.postArray} onDelete={this.onDelete} onSubmit={(event, arg) => this.onSubmit(arg, event)} />
+        <PostDisplayList postList={this.state.postArray} onDelete={this.onDelete} onSubmit={(event, arg) => this.onSubmit(arg, event)}  onCheckboxChange={this.onCheckboxChange}/>
       </div>
     );
   }
